b13d44203f1c9f2fed0012a92efebf7dc99c17a0
Dusky7 <victorsantos12dima@gmail.com>
2023-05-20 02:02:15 -0300

    Aula 2

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,14 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+
+import { memoriesRoutes } from './routes/memories'
+import cors from '@fastify/cors'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
+app.register(cors, {
+  origin: true, //todas URLs de front-end poderão acessar nosso back-end
 })
+app.register(memoriesRoutes)
 
 app
   .listen({
@@ -17,3 +17,5 @@ app
   .then(() => {
     console.log('🚀 Http server running on http://localhost:3333')
   })
+
+  
